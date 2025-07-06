86d0377a3d42d490d0ad9ce82631e66a0331f477
pgrasic <pgrasic98@gmail.com>
2025-07-06 11:24:50 +0200

    statistics added

--- src/services/TicketService.py
+++ src/services/TicketService.py
@@ -1,6 +1,7 @@
 from typing import Any
 import httpx
 from models.Ticket import Ticket, transform_ticket
+from collections import Counter
 
 
 async def get_user(user_id: int):
@@ -65,3 +66,36 @@ async def get_tickets_search(naziv:str) -> dict[str, Any]:
         return {
         "tickets": searched_tickets,
     }
+
+async def stats():
+        async with httpx.AsyncClient() as client:
+            fetched_tickets = await fetch_tickets(client)
+            total = len(Ticket)
+            status_counter=Counter(t.status for t in fetched_tickets)
+            priority_counter=Counter(t.priority for t in fetched_tickets)
+            ticket_per_user=Counter(t.asignee for t in fetched_tickets)
+            most_loaded_user = None
+            max_tickets = 0
+            for user, count in ticket_per_user.items():
+                if count > max_tickets:
+                    max_tickets = count
+                    most_loaded_user = user
+
+            ratio_closed= round((status_counter.get("closed", None)/total)*100, 2)
+            most_common_status = status_counter.most_common(1)[0][0]
+            priority_relative = {}
+            for priority, count in priority_counter.items():
+                relative_freq = round(count / total, 3)
+                priority_relative[priority] = relative_freq
+
+
+            
+            return{
+            "total":total,
+            "status_counter": dict(status_counter),
+            "priority_counter":dict(priority_counter),
+            "ticket_per_user":dict(ticket_per_user),
+            "ratio_closed": ratio_closed,
+            "priority_relative": relative_freq,
+            "most_common_status": most_common_status
+            }
